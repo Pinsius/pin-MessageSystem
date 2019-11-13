c2fa89d28d2d161c8fc7188a9ade38bae3a09f04
Pinsius <lubonnt@gmail.com>
2019-11-13 22:54:35 +0100

    new demo uploaded

--- main.cpp
+++ main.cpp
@@ -4,38 +4,92 @@
 
 class Foo
 {
-	public:
+private:
+	
+	int id;
 
-		void test()
-		{
-			std::cout << "test function is being called" << std::endl;
-		}
+public:
+	pin::MessageHandler handler;
 
-		void test_int(int number)
-		{
-			std::cout << "Value is: " << number << std::endl;
-		}
+	void init(pin::MessageManager* manager)
+	{
+		handler.initHandler(manager);
 
+		//Listen to two types of messages
+		handler.listenToMessage("NoParameterMessage", &Foo::info, this);
+		handler.listenToMessage("IntFloatMessage", &Foo::intFloatFunction, this);
+	}
+
+	void info()
+	{
+		std::cout << "Foo info recieved" << std::endl;
+	}
+
+	void intFloatFunction(int i, float f)
+	{
+		std::cout << "Integer is " << i << " and float " << f << std::endl;
+	}
+
+	int getId()
+	{
+		return id;
+	}
+
+	void setId(int Id)
+	{
+		id = Id;
+	}
 };
 
-int main(int argc, char** argv)
+class Foo2
 {
+private:
+	
+
+public:
+	pin::MessageHandler handler;
+
+	void init(pin::MessageManager* manager)
+	{
+		handler.initHandler(manager);
+
+		//Listen to two types of messages as well
+		handler.listenToMessage("NoParameterMessage", &Foo2::info, this);
+		handler.listenToMessage("CustomClassMessage", &Foo2::fooFunction, this);
+	}
+
+	void info()
+	{
+		std::cout << "Foo2 info recieved" << std::endl;
+	}
+
+	void fooFunction(Foo* f)
+	{
+		std::cout << "id is " << f->getId() << std::endl;
+	}
+};
+
+
+int main()
+{
+	pin::MessageManager manager;
+
+	manager.createMessage<>("NoParameterMessage");
+	manager.createMessage<int, float>("IntFloatMessage");
+	manager.createMessage<Foo*>("CustomClassMessage");
+
 	Foo foo;
+	foo.init(&manager);
+	foo.setId(100);
 
-	pin::MessageHandler tut;
+	Foo2 foo2;
+	foo2.init(&manager);
 
-
-	pin::MessageManager::createMessage<>("Blank message");
-	pin::MessageManager::createMessage<int>("Int message");
-
-	tut.listenToMessage("Blank message", &Foo::test, &foo);
-	tut.listenToMessage("Int message", &Foo::test_int, &foo);
-
-	tut.broadcastMessage("Blank message");
-	tut.broadcastMessage("Int message", 5);
-
+	foo.handler.broadcastMessage("NoParameterMessage");
+	foo2.handler.broadcastMessage("CustomClassMessage", &foo);
+	foo.handler.broadcastMessage("IntFloatMessage", 5, 42.5f);
 
 	system("pause");
-
 	return 0;
+
 }
